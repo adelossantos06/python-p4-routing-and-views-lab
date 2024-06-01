4fb034a62df3846ff18e50ba3727307791c540b1
Alyssa De Los Santos <adelossantos6@gmail.com>
2024-06-01 16:31:21 -0500

    complete lab routes and view lab

--- server/app.py
+++ server/app.py
@@ -4,5 +4,40 @@ from flask import Flask
 
 app = Flask(__name__)
 
+@app.route('/')
+def index():
+    return '<h1>Python Operations with Flask Routing and Views</h1>'
+
+@app.route('/print/<string:string>')
+def print_string(string):
+    print(f'{string}')
+    return f'{string}'
+
+@app.route('/count/<int:count>')
+def count(count):
+    result = ''
+    for i in range(0, count):
+        result += f'{i}\n'
+    return result
+
+@app.route('/math/<int:num1>/<operation>/<int:num2>')
+def math(num1, operation, num2):
+    if operation == '+':
+        add = num1 + num2
+        return f"{add}"
+    elif operation == '-':
+        subtract = num1 - num2
+        return f"{subtract}"
+    elif operation == '*':
+        multiply = num1 * num2
+        return f"{multiply}"
+    elif operation == 'div':
+        divide = num1 / num2
+        return f"{divide}"
+    elif operation == '%':
+        remainder = num1 % num2
+        return f"{remainder}"
+    
+
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
